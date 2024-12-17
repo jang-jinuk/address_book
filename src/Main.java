c71c3c0b6f7a3e7d1dcd77228df9a611e560c5ec
kaydeen <153696777+doopang24@users.noreply.github.com>
2024-12-17 11:42:04 +0900

    feat: main() 클래스 수정

--- src/Main.java
+++ src/Main.java
@@ -5,36 +5,46 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Main main = new Main();
-    main.readCVS();
+
+//    main.readCVS();
   }
 
-  public List<List<String>> readCVS() {
-    List<List<String>> cvsList = new ArrayList<List<String>>();
-    File csv = new File("C:\\intelij\\CodeSquad-Test\\address_book\\name,adress,number.csv");
-    BufferedReader br = null;
-    String line = "";
-
-    try{
-      br = new BufferedReader(new FileReader(csv));
-      while ((line = br.readLine()) != null) {
-        List<String> aLine = new ArrayList<String>();
-        String[] lineArr = line.split(",");
-        aLine = Arrays.asList(lineArr);
-        cvsList.add(aLine);
-      }
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      try {
-        if (br != null) {
-          br.close();
-        }
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
-    return cvsList;
+  public void showManu() {
+    System.out.println("사용하고 싶은 기능의 번호를 입력하세요.");
   }
+
+  public void printOpeningMessage() {
+    System.out.println("주소록 프로그램을 시작합니다.");
+    System.out.println("--------------------------");
+  }
+
+//  public List<List<String>> readCVS() {
+//    List<List<String>> cvsList = new ArrayList<List<String>>();
+//    File csv = new File("C:\\intelij\\CodeSquad-Test\\address_book\\name,adress,number.csv");
+//    BufferedReader br = null;
+//    String line = "";
+//
+//    try{
+//      br = new BufferedReader(new FileReader(csv));
+//      while ((line = br.readLine()) != null) {
+//        List<String> aLine = new ArrayList<String>();
+//        String[] lineArr = line.split(",");
+//        aLine = Arrays.asList(lineArr);
+//        cvsList.add(aLine);
+//      }
+//    } catch (FileNotFoundException e) {
+//      e.printStackTrace();
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    } finally {
+//      try {
+//        if (br != null) {
+//          br.close();
+//        }
+//      } catch (IOException e) {
+//        e.printStackTrace();
+//      }
+//    }
+//    return cvsList;
+//  }
 }
