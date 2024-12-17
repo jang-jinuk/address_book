4074949d813b638d37ae66ed249a18120d1aeb53
kaydeen <153696777+doopang24@users.noreply.github.com>
2024-12-17 12:05:34 +0900

    refactor: switch문을 if문으로 변경

--- src/Main.java
+++ src/Main.java
@@ -18,16 +18,20 @@ public class Main {
 
   public void addressBookController() {
     int input = 0;
-    while (input == 4) {
+    while (true) {
       showManu();
       input = getInput();
-      switch (input) {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-          printEndingMessage();
+      if(input == 1) {
+
+      } else if(input == 2) {
+
+      } else if(input == 3) {
+
+      } else {
+        printEndingMessage();
+        return;
       }
+
     }
   }
 
