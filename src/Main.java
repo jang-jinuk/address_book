0a76ebc19a71322b1360e54c7503b46b7b090f90
kaydeen <153696777+doopang24@users.noreply.github.com>
2024-12-17 13:00:14 +0900

    refactor: Main 클래스 수정

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.io.*;
-import java.util.*;
 import java.util.Scanner;
 
 public class Main {
@@ -73,35 +71,4 @@ public class Main {
         String userNumber = scanner.nextLine();
         return userNumber;
     }
-
-
-//  public List<List<String>> readCVS() {
-//    List<List<String>> cvsList = new ArrayList<List<String>>();
-//    File csv = new File("C:\\intelij\\CodeSquad-Test\\address_book\\name,adress,number.csv");
-//    BufferedReader br = null;
-//    String line = "";
-//
-//    try{
-//      br = new BufferedReader(new FileReader(csv));
-//      while ((line = br.readLine()) != null) {
-//        List<String> aLine = new ArrayList<String>();
-//        String[] lineArr = line.split(",");
-//        aLine = Arrays.asList(lineArr);
-//        cvsList.add(aLine);
-//      }
-//    } catch (FileNotFoundException e) {
-//      e.printStackTrace();
-//    } catch (IOException e) {
-//      e.printStackTrace();
-//    } finally {
-//      try {
-//        if (br != null) {
-//          br.close();
-//        }
-//      } catch (IOException e) {
-//        e.printStackTrace();
-//      }
-//    }
-//    return cvsList;
-//  }
 }
