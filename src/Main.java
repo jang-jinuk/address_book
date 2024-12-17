0fcbbbfe3aa86c7c90185160c9b0a5d6b50e6aad
kaydeen <153696777+doopang24@users.noreply.github.com>
2024-12-17 11:45:20 +0900

    refactor: 메뉴 출력 기능 추가

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,22 @@ import java.util.*;
 public class Main {
   public static void main(String[] args) {
     Main main = new Main();
+    main.printOpeningMessage();
+    main.showManu();
 
 //    main.readCVS();
   }
 
   public void showManu() {
     System.out.println("사용하고 싶은 기능의 번호를 입력하세요.");
+    System.out.println("1. 데이터 읽기\n2. 데이터 추가\n3. 데이터 초기화\n4. 종료");
   }
 
   public void printOpeningMessage() {
     System.out.println("주소록 프로그램을 시작합니다.");
     System.out.println("--------------------------");
   }
+
 
 //  public List<List<String>> readCVS() {
 //    List<List<String>> cvsList = new ArrayList<List<String>>();
