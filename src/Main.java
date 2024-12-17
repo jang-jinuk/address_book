6a6b35b514c4848772a597abf4ec3d46b592351d
kaydeen <153696777+doopang24@users.noreply.github.com>
2024-12-17 11:25:29 +0900

    feat: 출력 기능

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Main main = new Main();
+    main.readCVS();
+  }
+
+  public List<List<String>> readCVS() {
+    List<List<String>> cvsList = new ArrayList<List<String>>();
+    File csv = new File("C:\\intelij\\CodeSquad-Test\\address_book\\name,adress,number.csv");
+    BufferedReader br = null;
+    String line = "";
+
+    try{
+      br = new BufferedReader(new FileReader(csv));
+      while ((line = br.readLine()) != null) {
+        List<String> aLine = new ArrayList<String>();
+        String[] lineArr = line.split(",");
+        aLine = Arrays.asList(lineArr);
+        cvsList.add(aLine);
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (br != null) {
+          br.close();
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    return cvsList;
   }
 }
